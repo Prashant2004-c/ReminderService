521782f68ca1cfdf5c7ccd3cbd1428255e14b8ee
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-02-20 02:23:38 +0530

    basic messageQueues setup done

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,5 +5,8 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     email_id: process.env.email_id,
-    email_password: process.env.email_pass
+    email_password: process.env.email_pass,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY,
+    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL
 }
--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ const { PORT } = require('./config/serverConfig');
 const { sendBasicEmail } = require('./services/email-service');
 const jobs = require('./utils/job');
 const TicketController = require('./controllers/ticket-controller');
+const { createChannel   } = require('./utils/messageQueue');
 
 
-const setupAndStartServer = () => {
+
+const setupAndStartServer = async () => {
     const app = express();
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
     app.post('/api/v1/tickets', TicketController.create); 
+
+    const channel = await createChannel();
 
     app.listen(PORT , () => {
         console.log(`Server started on port ${PORT}`);
@@ -24,7 +27,7 @@ const setupAndStartServer = () => {
         //     'This is a testing mail',
         //     'Hi, how are you we hope you will enjoy the journey.'
         // )
-        jobs();
+        // jobs();
     })
 }
 
